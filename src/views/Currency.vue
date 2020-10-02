fe337d5244af50ecc839c607051f31c037b90e2e
masashinakayama <masashiii0115@gmail.com>
2020-10-02 20:52:44 +0900

    不具合修正

--- src/views/Currency.vue
+++ src/views/Currency.vue
@@ -1,21 +1,22 @@
 <template>
-  <el-row>
-    <el-col :span="24">
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>仮想通貨一覧</span>
-        </div>
-        <div>...</div>
-      </el-card>
-    </el-col>
-  </el-row>
+    <el-row>
+        <el-col :span="24">
+            <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>仮想通貨一覧</span>
+                </div>
+                <div>...</div>
+            </el-card>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
 export default {
-  name: "Currency",
-};
+  name: 'Currency'
+}
 </script>
 
 <style scoped>
+
 </style>
