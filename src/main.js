81e2972eda2f0eb9e5d1da56d0624ae3193e518b
masashinakayama <masashiii0115@gmail.com>
2020-10-03 12:52:45 +0900

    Firebaseとの連携

--- src/main.js
+++ src/main.js
@@ -7,8 +7,20 @@ import ElementUI from 'element-ui' // 追記
 import locale from 'element-ui/lib/locale/lang/ja' // 追記
 import 'element-ui/lib/theme-chalk/index.css' // 追記
 
+import firebase from 'firebase' // 追記
+
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale }) // 追記
+
+var config = {
+  apiKey: 'AIzaSyBIspLSnBaJzsrqYHFLVGjySTGLE5vbJNM',
+  authDomain: 'webapp-38b61.firebaseapp.com',
+  databaseURL: 'https://webapp-38b61.firebaseio.com',
+  projectId: 'webapp-38b61',
+  storageBucket: 'webapp-38b61.appspot.com',
+  messagingSenderId: '351774348336'
+}
+firebase.initializeApp(config)
 
 new Vue({
   router,
